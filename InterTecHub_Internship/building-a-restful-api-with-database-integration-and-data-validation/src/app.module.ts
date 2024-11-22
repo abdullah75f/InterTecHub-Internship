157f6eaf3938e5ae800712f2738d6aeb7dfbc338
Abdullah.F <abdullah75farid@gmail.com>
2024-11-22 14:50:36 +0300

    code structure

--- InterTecHub_Internship/building-a-restful-api-with-database-integration-and-data-validation/src/app.module.ts
+++ InterTecHub_Internship/building-a-restful-api-with-database-integration-and-data-validation/src/app.module.ts
@@ -1,9 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        ssl: { rejectUnauthorized: false },
+        autLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    UsersModule,
+    BooksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
