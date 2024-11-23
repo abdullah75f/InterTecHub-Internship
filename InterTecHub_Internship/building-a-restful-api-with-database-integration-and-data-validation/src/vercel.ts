956ba0e42c2617cebd67142ff61c372b5d4dde78
Abdullah.F <abdullah75farid@gmail.com>
2024-11-23 14:52:57 +0300

    setup

--- InterTecHub_Internship/building-a-restful-api-with-database-integration-and-data-validation/src/vercel.ts
+++ InterTecHub_Internship/building-a-restful-api-with-database-integration-and-data-validation/src/vercel.ts
@@ -1,25 +1,15 @@
-import { createServer, proxy } from 'aws-serverless-express';
-import { Server } from 'http';
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express';
 import { AppModule } from './app.module';
 
-const binaryMimeTypes: string[] = [];
+const server = express();
 
-let cachedServer: Server;
-
-const bootstrapServer = async (): Promise<Server> => {
-  if (!cachedServer) {
-    const expressApp = express();
-    const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
-    await nestApp.init();
-    cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
-  }
-  return cachedServer;
+const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressInstance));
+  await app.init();
 };
 
-export const handler = async (event: any, context: any) => {
-  cachedServer = await bootstrapServer();
-  return proxy(cachedServer, event, context, 'PROMISE').promise;
-};
+createNestServer(server);
+
+export default server;
