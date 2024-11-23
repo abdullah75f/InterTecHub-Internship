72621df59da3df811ccccc441f8d9eaa602bcbc2
Abdullah.F <abdullah75farid@gmail.com>
2024-11-23 14:36:57 +0300

    deployment setup

--- InterTecHub_Internship/building-a-restful-api-with-database-integration-and-data-validation/src/vercel.ts
+++ InterTecHub_Internship/building-a-restful-api-with-database-integration-and-data-validation/src/vercel.ts
@@ -1,15 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express'; // Use default import for express
+import express from 'express';
 import { AppModule } from './app.module';
 
 const server = express();
 
 const createNestServer = async (expressInstance) => {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressInstance),
-  );
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressInstance));
   await app.init();
 };
 
