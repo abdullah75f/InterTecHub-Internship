e98ca819792e7a6856c31eb073e423c16be63aaf
Abdullah.F <abdullah75farid@gmail.com>
2024-11-23 21:56:03 +0300

    deployment

--- InterTecHub_Internship/building-a-restful-api-with-database-integration-and-data-validation/src/main.ts
+++ InterTecHub_Internship/building-a-restful-api-with-database-integration-and-data-validation/src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+
+// Vercel handler
+let server: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,5 +14,21 @@ async function bootstrap() {
     }),
   );
   await app.listen(process.env.PORT ?? 3000);
+
+  // Enable CORS if needed
+  app.enableCors();
+
+  await app.init();
+  server = createServer((req: IncomingMessage, res: ServerResponse) => {
+    app.getHttpAdapter().getInstance()(req, res);
+  });
 }
 bootstrap();
+
+export default (req: IncomingMessage, res: ServerResponse) => {
+  if (!server) {
+    bootstrap().then(() => server.emit('request', req, res));
+  } else {
+    server.emit('request', req, res);
+  }
+};
