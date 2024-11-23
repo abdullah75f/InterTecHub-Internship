1ad47887ecc5c90a1d63f63560372be188423489
Abdullah.F <abdullah75farid@gmail.com>
2024-11-23 22:05:22 +0300

    deployment 😮‍💨

--- InterTecHub_Internship/building-a-restful-api-with-database-integration-and-data-validation/src/main.ts
+++ InterTecHub_Internship/building-a-restful-api-with-database-integration-and-data-validation/src/main.ts
@@ -3,32 +3,31 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { createServer, IncomingMessage, ServerResponse } from 'http';
 
-// Vercel handler
 let server: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS before app initialization
+  app.enableCors();
+
+  // Apply global validation pipe
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
-
-  // Enable CORS if needed
-  app.enableCors();
 
   await app.init();
+
   server = createServer((req: IncomingMessage, res: ServerResponse) => {
     app.getHttpAdapter().getInstance()(req, res);
   });
 }
-bootstrap();
 
-export default (req: IncomingMessage, res: ServerResponse) => {
+export default async (req: IncomingMessage, res: ServerResponse) => {
   if (!server) {
-    bootstrap().then(() => server.emit('request', req, res));
-  } else {
-    server.emit('request', req, res);
+    await bootstrap();
   }
+  server.emit('request', req, res);
 };
