8eaedecce841a358c1b6c933620e668293e537a9
Abdullah.F <abdullah75farid@gmail.com>
2024-11-23 14:28:02 +0300

    setup

---
+++ InterTecHub_Internship/building-a-restful-api-with-database-integration-and-data-validation/src/vercel.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import { AppModule } from './app.module';
+
+const server = express();
+
+const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  await app.init();
+};
+
+createNestServer(server);
+
+export default server;
