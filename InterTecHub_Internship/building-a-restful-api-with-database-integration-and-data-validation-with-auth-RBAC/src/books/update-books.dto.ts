ca67fbb790a2d6daa9466afbba1e63fb3e6c0ff3
Abdullah.F <abdullah75farid@gmail.com>
2024-12-06 23:03:02 +0300

    project structure for task-3

--- InterTecHub_Internship/building-a-restful-api-with-database-integration-and-data-validation-with-auth-RBAC/src/books/update-books.dto.ts
+++
@@ -1,4 +0,0 @@
-import { CreateBookDto } from './create-books.dto';
-import { PartialType } from '@nestjs/mapped-types';
-
-export class UpdateBooksDto extends PartialType(CreateBookDto) {}
--- InterTecHub_Internship/building-a-restful-api-with-database-integration-and-data-validation-with-auth-RBAC/src/reviews/update-reviews.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateReviewsDto } from './create-reviews.dto';
-
-export class UpdateReviewsDto extends PartialType(CreateReviewsDto) {}
