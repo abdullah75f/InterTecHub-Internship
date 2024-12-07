73223fe60d229b8459bcc518a705a5808541bcee
Abdullah.F <abdullah75farid@gmail.com>
2024-12-07 13:25:28 +0300

    hashed and salted password for signup

--- InterTecHub_Internship/advanced-API-with-authentication-and-role-based-access-control/src/users/auth.service.ts
+++ InterTecHub_Internship/advanced-API-with-authentication-and-role-based-access-control/src/users/auth.service.ts
@@ -1,12 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes } from 'crypto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly usersService: UsersService) {}
 
-  // async signup(email: string, password: string) {
-  //   const existingUser =
-  // }
-  // async login() {}
+  async signup(email: string, password: string, name: string) {
+    const existingUser = await this.usersService.find(email);
+    if (existingUser.length) {
+      throw new BadRequestException('Email is in use');
+    }
+
+    const salt = randomBytes(8).toString('hex');
+    const hashedPassword = (await bcrypt.hash(password, salt, 32)) as Buffer;
+
+    const result = salt + '.' + hashedPassword.toString('hex');
+
+    const user = await this.usersService.createUser(email, result, name);
+    return user;
+  }
+  async login() {}
 }
