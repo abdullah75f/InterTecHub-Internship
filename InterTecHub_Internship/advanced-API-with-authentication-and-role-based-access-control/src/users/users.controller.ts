e939f482587239766bbf1c895264edfb9d5da9aa
Abdullah.F <abdullah75farid@gmail.com>
2024-12-07 12:05:41 +0300

    user-controller

--- InterTecHub_Internship/advanced-API-with-authentication-and-role-based-access-control/src/users/users.controller.ts
+++ InterTecHub_Internship/advanced-API-with-authentication-and-role-based-access-control/src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './create-user.dto';
 
 @Controller('users')
 export class UsersController {
   @Post()
-  createUser() {}
+  createUser(@Body() user: CreateUserDto) {}
 }
