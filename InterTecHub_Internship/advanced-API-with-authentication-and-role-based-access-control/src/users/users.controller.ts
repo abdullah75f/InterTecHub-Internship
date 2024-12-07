7cd561cb8a4dce8987510eba01a56232e198e270
Abdullah.F <abdullah75farid@gmail.com>
2024-12-07 13:05:26 +0300

    Auth service logic design

--- InterTecHub_Internship/advanced-API-with-authentication-and-role-based-access-control/src/users/users.controller.ts
+++ InterTecHub_Internship/advanced-API-with-authentication-and-role-based-access-control/src/users/users.controller.ts
@@ -1,8 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './create-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
+@Controller('auth')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {}
+  createUser(@Body() body: CreateUserDto) {
+    return this.usersService.createUser(body.email, body.name, body.password);
+  }
 }
