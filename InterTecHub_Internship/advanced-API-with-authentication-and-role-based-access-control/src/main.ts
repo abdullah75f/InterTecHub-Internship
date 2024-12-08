e791276a3ad0493f080cda836930e8b9d62f5a9d
Abdullah.F <abdullah75farid@gmail.com>
2024-12-08 22:32:51 +0300

    main.ts

--- InterTecHub_Internship/advanced-API-with-authentication-and-role-based-access-control/src/main.ts
+++ InterTecHub_Internship/advanced-API-with-authentication-and-role-based-access-control/src/main.ts
@@ -1,20 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://intertechub-internship.onrender.com', // Allow specific origin
-    methods: 'GET,POST,PUT,DELETE,OPTIONS', // Allow specific methods
-    allowedHeaders: 'Content-Type, Authorization', // Allow specific headers
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-  });
+
+  // Use Express JSON middleware
+  app.use(express.json());
+
+  // Use Express CORS middleware
+  app.use(
+    cors({
+      origin: 'https://intertechub-internship.onrender.com',
+      methods: 'GET,POST,PUT,DELETE,OPTIONS',
+      allowedHeaders: 'Content-Type, Authorization',
+      credentials: true,
+    }),
+  );
+
+  // Use global validation pipe
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
