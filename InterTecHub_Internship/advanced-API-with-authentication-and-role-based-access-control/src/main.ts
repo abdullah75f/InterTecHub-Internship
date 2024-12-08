a2bd971b98ca43b6aafe0aeb15c0324f95460327
Abdullah.F <abdullah75farid@gmail.com>
2024-12-08 22:06:23 +0300

    main.ts

--- InterTecHub_Internship/advanced-API-with-authentication-and-role-based-access-control/src/main.ts
+++ InterTecHub_Internship/advanced-API-with-authentication-and-role-based-access-control/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'https://intertechub-internship.onrender.com',
-    methods: 'GET,POST,PUT,DELETE',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
