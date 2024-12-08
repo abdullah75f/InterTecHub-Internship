642a2e0fb728c30f02a941a4e35d339910acfff4
Abdullah.F <abdullah75farid@gmail.com>
2024-12-08 22:05:54 +0300

    main.ts

--- InterTecHub_Internship/advanced-API-with-authentication-and-role-based-access-control/src/main.ts
+++ InterTecHub_Internship/advanced-API-with-authentication-and-role-based-access-control/src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
     origin: 'https://intertechub-internship.onrender.com',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
   });
   app.useGlobalPipes(
     new ValidationPipe({
